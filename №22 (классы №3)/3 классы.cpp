d240a0c4739294e3f39f5951ac5ee74ed79ac7e0
maks1401 <63349353+maks1401@users.noreply.github.com>
2020-04-14 21:49:34 +0500

    №22 (классы №3)

---
+++ №22 (классы №3)/3 классы.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include "Money.h"
+
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	int k;
+	Money a;
+	Money b;
+	Money c;
+	Money d;
+	cin >> a;
+	cin >> b;
+	if (a > b)
+		c = a;
+	else if (a < b)
+		c = b;
+	else
+		c = a + b;
+	cout << "a == " << a << endl;
+	cout << "b == " << b << endl;
+	cout << "MAX == " << c << endl;
+	cout << "Сколько копеек добавить к a: ";
+	cin >> k;
+	for (int i = 0; i < k; i++)
+	{
+		a++;
+	}
+	cout << "a == " << a << endl;
+}
