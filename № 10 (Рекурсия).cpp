84d6a4f4c78737711a7d8c104a4035765bc847fe
maks1401 <63349353+maks1401@users.noreply.github.com>
2020-04-14 18:24:45 +0500

    № 10 (Рекурсия)

---
+++ № 10 (Рекурсия).cpp
@@ -0,0 +1,15 @@
+﻿//Трегубов Максим, лаб. №10, рекурсия
+#include <iostream>
+ 
+int get_count(int n, int prev = 0) 
+{
+    if(n != 0)
+        return prev ? get_count(n - 1, 0) : get_count(n - 1, 0) + get_count(n - 1, 1);   
+    else 
+        return 1;
+}
+ 
+int main()
+{
+    std::cout << get_count(3);
+}
