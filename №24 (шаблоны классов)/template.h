ce52a2b3e586e44f8df50c524072f2c026c27ad3
Максим Трегубов <63349353+maks1401@users.noreply.github.com>
2020-04-16 23:49:20 +0500

    №24 (шаблоны классов)

---
+++ №24 (шаблоны классов)/template.h
@@ -0,0 +1,94 @@
+#pragma once
+#include <iostream>
+using namespace std;
+template <class T> //T - параметр шаблона
+class set
+{
+public:
+	set(int s, T k);
+	~set();
+	set(const set<T>& a);
+	set& operator=(const set<T>& a);
+	T& operator[](int index);
+	set operator+(set<T>& a);
+	friend ostream& operator<< <>(ostream& out, const set<T>& a);
+	friend istream& operator>> <>(istream& in, set<T>& a);
+private:
+	int elem;
+	T* data;
+};
+// опрeделение функций
+//конструктор с параметрами
+template <class T>
+set<T>::set(int n, T k)
+{
+	elem = n;
+	data = new T[elem];
+	for (int i = 0; i < elem; i++)
+		data[i] = k;
+
+}
+//конструктор копирования
+template <class T>
+set<T>::set(const set & a)
+{
+	elem = a.elem;
+	data = new T[elem];
+	for (int i = 0; i < elem; i++)
+		data[i] = a.data[i];
+
+}
+//деструктор
+template <class T>
+set<T>::~set()
+
+{
+	delete[]data;
+	data = 0;
+}
+//операция присваивания
+template <class T>
+set<T>& set<T>::operator=(const set<T> & a)
+{
+	if (this == &a)
+		return *this;
+	elem = a.elem;
+	if (data != 0)
+		delete[]data;
+	data = new T[elem];
+	for (int i = 0; i < elem; i++)
+		data[i] = a.data[i];
+	return *this;
+}
+//операция доступа по индексу
+template <class T>
+T & set<T>::operator[](int index)
+{
+	if (index < elem) return data[index];
+	else cout << "\nОшибка: Индекс больше количества элементов";
+}
+//операции для ввода-вывода
+template <class T>
+ostream& operator<< (ostream & out, const set<T> & a)
+{
+	for (int i = 0; i < a.elem; ++i)
+		out << a.data[i] << " ";
+	return out;
+}
+template <class T>
+istream& operator>> (istream & in, set<T> & a)
+{
+	for (int i = 0; i < a.elem; ++i)
+		in >> a.data[i];
+	return in;
+}
+template <class T>
+set<T> set<T>::operator+(set<T>& a)
+{
+	for (int i = 0; i < elem; i++)
+	{
+		data[i] = data[i] + a.data[i];
+	}
+	return *this;
+}
+
---
+++ №24 (шаблоны классов)/№24 (шаблоны классов).cpp
@@ -0,0 +1,46 @@
+﻿//Трегубов Максим, лаб. №24, шаблоны классов
+#include "list.h"
+#include "template.h"
+#include "Money.h"
+#include <iostream>
+using namespace std;
+
+void main()
+{
+	setlocale(LC_ALL, "rus");
+	cout << "Введите пять элементов списка 'a': ";
+	cout << '\n';
+	set<int>a(5, 0);
+	cin >> a;
+	cout << a << endl;
+	cout << "Список 'b': ";
+	set<int>b(5,2);
+	cout << b << endl;
+	set<int>c(5, 0);
+	cout << "Тогда список 'с=a+b': ";
+	c = a + b;
+	cout << c << endl;
+	int s;
+	cout << "Введите константу, которую хотите добавить к 'с': ";
+	cin >> s;
+	set<int>d(5, s);
+	cout << "Тогда список 'с': ";
+	c = c + d;
+	cout << c << endl;	
+	
+	Money t;
+	cin >> t;
+	cout << t << endl;
+	Money q;
+	cin >> q;
+	cout << q << endl;
+	
+	cout << "Введите две суммы: "<<endl;
+	set<Money>R(2,q);
+	cin >> R;
+	cout << R ;
+	
+	cout<< endl <<"Доступ по индексу 1: "<< R[1];//доступ по индексу
+	   
+
+}
